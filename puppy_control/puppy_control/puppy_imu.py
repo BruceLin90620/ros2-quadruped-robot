30c3ec4de7b6b1fe8a2ecd98f2b12ea28932ba38
BruceLin90620 <brucelin90620@gmail.com>
2023-11-24 15:31:59 +0800

    change pwd

--- puppy_control/puppy_control/puppy_imu.py
+++ puppy_control/puppy_control/puppy_imu.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 
 from sensor_msgs.msg import Imu
 import sys
-sys.path.append('/home/puppypi/puppypi_ws/quadruped-robot-ros2/puppy_control/driver')
+sys.path.append('/home/ubuntu/puppypi_ws/quadruped-robot-ros2/puppy_control/driver')
 from MPU6050 import MPU6050Base, KalmanIMU
 
 class MPU6050Publisher(Node):
